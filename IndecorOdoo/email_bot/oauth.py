cdcd7a707336c61b1bc89d5be718b82ff6237613
BenjaminEchelmeier <benjaminechelmeier@gmail.com>
2021-08-31 14:16:04 -0400

    full base gmail functionality completed

--- IndecorOdoo/email_bot/oauth.py
+++ IndecorOdoo/email_bot/oauth.py
@@ -1,14 +1,16 @@
 from pathlib import Path
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.auth.exceptions import RefreshError
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from json import JSONDecodeError
 import email
-from base64 import urlsafe_b64decode
-
-
+import time
+from base64 import urlsafe_b64decode, urlsafe_b64encode
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['modify']
 CRED_PATH = Path('/home/benjamin/predicatestudio/ps-sandbox/IndecorOdoo/email_bot/IndecorEmailBot.json')
@@ -84,7 +86,7 @@ class OAuthGmailUser():
         if self._service:
             try:
                 return bool(self._service.users().getProfile(userId=self.user_id).execute())
-            except HttpError:
+            except (HttpError, RefreshError):
                 #TODO any other errors that can be returned here?
                 return False
         else:
@@ -106,7 +108,121 @@ class OAuthGmailUser():
 
     def get_messages(self, labels=None, query=None, page_token=None, max_results = None, include_spam_and_trash = None):
         return [self.get_message(message['id']) for message in self.list_messages(labels=labels, query=query, page_token=page_token, max_results = max_results, include_spam_and_trash = include_spam_and_trash)['messages']]
+    def create_message(self, to: str, subject: str, message_text: str, file: str = None):
+        if not file:
+            message = self._create_text_message(to, subject, message_text)
+        else:
+            message = self._create_attachment_message(to, subject, message_text, file)
+        # Not making a new subclass just for this attirbute but need to initialize the variable for replies
+        message.threadId = None
+        return message
 
+    def create_reply(self, reply_to: 'GMessage', message_text: str, file: str = None):
+        subject = reply_to.mime["Subject"]
+        if "Re: " != subject[:4]:
+            subject = "Re: " + subject
+        to=reply_to.mime["From"]
+        reply_mes = self.create_message(to=to, subject=subject, message_text=message_text, file=file)
+        
+        reply_mes.threadId = reply_to.threadId
+
+        reply_id = reply_to.mime.get("Message-ID", "")
+        references = reply_to.mime.get("References")
+        if not references:
+            references = reply_to.mime["In-Reply-To"]
+        if references and reply_id:
+            references += f"\t{reply_id}"
+
+        del reply_mes["In-Reply-To"]
+        del reply_mes["References"]
+
+        reply_mes["In-Reply-To"] = reply_id
+        reply_mes["References"] = references
+
+        return reply_mes
+    
+    def _create_text_message(self, to: str, subject: str, message_text: str):
+        """Create a message for an email.
+
+        Args:
+            to: Email address of the receiver.
+            subject: The subject of the email message.
+            message_text: The text of the email message.
+
+        Returns:
+            An object containing a base64url encoded email object.
+        """
+        message = email.mime.text.MIMEText(message_text)
+        message['to'] = to
+        message['from'] = self.user_id
+        message['subject'] = subject
+        return message
+    
+    def _create_attachment_message(self, to: str, subject: str, message_text: str, file: str):
+        """Create a message for an email.
+
+        Args:
+            to: Email address of the receiver.
+            subject: The subject of the email message.
+            message_text: The text of the email message.
+            file: The path to the file to be attached.
+
+        Returns:
+            An object containing a base64url encoded email object.
+        """
+        message = MIMEMultipart()
+        message['to'] = to
+        message['from'] = self.user_id
+        message['subject'] = subject
+
+        msg = MIMEText(message_text)
+        message.attach(msg)
+
+        content_type, encoding = mimetypes.guess_type(file)
+
+        if content_type is None or encoding is not None:
+            content_type = 'application/octet-stream'
+        main_type, sub_type = content_type.split('/', 1)
+        if main_type == 'text':
+            fp = open(file, 'rb')
+            msg = MIMEText(fp.read(), _subtype=sub_type)
+            fp.close()
+        elif main_type == 'image':
+            fp = open(file, 'rb')
+            msg = MIMEImage(fp.read(), _subtype=sub_type)
+            fp.close()
+        elif main_type == 'audio':
+            fp = open(file, 'rb')
+            msg = MIMEAudio(fp.read(), _subtype=sub_type)
+            fp.close()
+        else:
+            fp = open(file, 'rb')
+            msg = MIMEBase(main_type, sub_type)
+            msg.set_payload(fp.read())
+            fp.close()
+        filename = os.path.basename(file)
+        msg.add_header('Content-Disposition', 'attachment', filename=filename)
+        message.attach(msg)
+        
+        return message
+    
+    def send_message(self, message):
+        """Send an email message.
+
+        Args:
+            service: Authorized Gmail API service instance.
+            message: MIME Message to be sent.
+
+        Returns:
+            Sent Message.
+        """
+        
+        api_package = {'raw': urlsafe_b64encode(message.as_bytes()).decode()}
+        if message.threadId:
+            api_package["threadId"]= message.threadId
+        api_response = (self._messages_api().send(userId=self.user_id, body=api_package).execute())
+        return api_response
+ 
 
 class GMessage():
     def __init__(self, GmailUser: OAuthGmailUser, message_id):
@@ -124,35 +240,10 @@ class GMessage():
 
         bit_message = urlsafe_b64decode(self.raw)
         self.mime = email.parser.BytesParser(policy=email.policy.SMTP).parsebytes(bit_message)
-        self.headers = dict(self.mime.items())
-        self.subject = self.headers['Subject']
+        self.subject = self.mime.get('Subject')
         self.body = self.mime.get_body(('plain'))
         self.html_body = self.mime.get_body(('html', 'plain'))
     
     def __repr__(self):
-        return str((self.id, self.subject))
-        
+        return str((self.id, self.subject, self.snippet))
 
-# def main():
-#     """Shows basic usage of the Gmail API.
-#     Lists the user's Gmail labels.
-#     """
-    
-#     creds = get_user_token(TOKEN_PATH, SCOPES)
-
-
-#     service = build('gmail', 'v1', credentials=creds)
-
-#     # Call the Gmail API
-#     results = service.users().labels().list(userId='me').execute()
-#     labels = results.get('labels', [])
-
-#     if not labels:
-#         print('No labels found.')
-#     else:
-#         print('Labels:')
-#         for label in labels:
-#             print(label['name'])
-
-# if __name__ == '__main__':
-#     main()
